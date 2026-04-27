8192d3c287226c604b28ec304c7a1a7bc79ad72e
pasmanul <seaman3d@gmail.com>
2026-04-27 17:04:43 +0900

    feat: load saved theme on startup

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BoardPage } from './ui/pages/BoardPage'
+import { useThemeStore } from './store/themeStore'
+
+// 保存済みテーマをCSS変数に適用してからレンダリング
+useThemeStore.getState().loadSavedTheme()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
